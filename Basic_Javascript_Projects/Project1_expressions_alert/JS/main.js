8e5ee4b7d2d86b06d856e7d5f12fe5b7761e4b5d
MurrinB <britneemurrin@yahoo.com>
2021-01-16 22:38:46 -0700

    Add comments

--- Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,17 @@
-var A = "This is a string."
-document.write(A)
+var A = "This is a string." //assign a value to a variable
+document.write(A)           //display variable
+var X = "I'M EXCITED TO LEARN JAVASCRIPT!" 
+window.alert(X)             // use variable to alert box content
+document.write(" Amanda said \"It's time for bed!\""); //used quotes to display on page
+document.write(" \"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind\"" //concatenated quote 
++ "-Dr. Seuss ");
+
+var Q = "Concatenated" + "String"
+document.write( Q); //concatenated with variable 
+
+var family = "The Fosters", mom = "Britnee", dad = "Richard", daughter = "Carma", son = "Keegan";
+var daughter = daughter.fontcolor("pink"); //assigned many variables and a color
+document.write(daughter);
+
+document.write(4+6); //used expression

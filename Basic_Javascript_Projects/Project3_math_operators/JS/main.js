f3dc0d0aa6c238b8c4789fc6da8e9f5e0bc74374
MurrinB <britneemurrin@yahoo.com>
2021-01-17 13:45:35 -0700

    Used math objects

--- Basic_Javascript_Projects/Project3_math_operators/JS/main.js
+++ Basic_Javascript_Projects/Project3_math_operators/JS/main.js
@@ -27,3 +27,25 @@ function multiple_math() {  //created a function with multiple math problems to 
     var simple_math = (4 + 2) * 5 / 2 - 6;
     document.getElementById("simple_math").innerHTML = "4 plus 2, multiplied by 5, divided in half and than subtracted by 6 equals " + simple_math;
 }
+
+function MandM() {    // created a function that will have a remainder
+    var K = 34 % 8;
+    document.getElementById("K").innerHTML = K + " is the remainder of 34 divided by 8"
+}
+
+function reesespieces() {  // created a function that returns the variable as a negative
+    var P = 16;
+    document.getElementById("P").innerHTML = -P;
+}
+
+var V = 10; //used variable and added 1 with ++
+V++;
+document.write(V);
+
+var R = 25; // used variable and subtracted 1 with --
+R--;
+document.write(R);
+
+window.alert(Math.random() * 10);   //made an alert box with random number between 0-10
+
+window.alert(Math.round (6.7));  //made an alert box with math object which allows me to preform mathmatical tasks on numbers

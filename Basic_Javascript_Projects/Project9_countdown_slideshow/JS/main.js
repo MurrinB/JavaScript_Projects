b77625d8574dc64e2707d02099baa390180457a8
MurrinB <britneemurrin@yahoo.com>
2021-01-28 16:16:22 -0700

    Completing challenges
    created auto slideshow

--- Basic_Javascript_Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic_Javascript_Projects/Project9_countdown_slideshow/JS/main.js
@@ -45,3 +45,14 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
     
 }
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+    $('#slideshow > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#slideshow');
+}, 3000);
